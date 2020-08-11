e252a6866799fd5151f6d9825a69d32873e1ca27
Adama <akabba@qa.com>
2020-08-11 13:22:26 +0100

    changed the text that is printed out

--- src/main/java/com/qa/day2/FizzBuzz.java
+++ src/main/java/com/qa/day2/FizzBuzz.java
@@ -1,8 +1,17 @@
+
 package com.qa.day2;
 
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        System.out.println("Idk");
+        System.out.println();
+        System.out.println();
+        System.out.println("dlkfjksdjfksdnfkjsdnfkjds");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
     }
 }
+
+
