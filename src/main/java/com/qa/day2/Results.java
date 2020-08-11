888a584a0276bb532190f5e8aa246505ddb024e9
Adama <akabba@qa.com>
2020-08-11 16:20:44 +0100

    radded print statements to display exam and total marks

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -6,4 +6,11 @@ public class Results {
     int biology;
     int total;
     int percentage;
+
+    public static void getMark(){
+        System.out.println("You got " + physics + "in your physics exam.");
+        System.out.println("You got " + chemistry + "in your chemistry exam.");
+        System.out.println("You got " + biology + "in your biology exam.");
+        System.out.println("You got" + total " in total.");
+    }
 }
