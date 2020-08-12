db41835172ac718feb780875befc6d36057efcb5
Adama <akabba@qa.com>
2020-08-12 11:20:53 +0100

    created flow chart one that iterates through value A until A = 200

---
+++ src/main/java/com/qa/day3/Flowcharts.java
@@ -0,0 +1,10 @@
+package com.qa.day3;
+
+public class Flowcharts {
+    public static void main(String[] args) {
+        for (int A = 100; A < 200 ; A++) {
+            System.out.println(A);
+        }
+    }
+
+}
