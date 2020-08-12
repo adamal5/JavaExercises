7d2b8f16b2ad16451b5a2fb50d8f494a6ed1b573
Adama <akabba@qa.com>
2020-08-12 12:08:32 +0100

    created a while loop that prints out number 1-10 ten times. CReated a for loop that prints out n number n times for numbers between 1-10

---
+++ src/main/java/com/qa/day3/Flowchart3.java
@@ -0,0 +1,27 @@
+package com.qa.day3;
+
+public class Flowchart3 {
+public static int number= 1;
+
+    public static void main(String[] args) {
+        /*
+        while (number <=10){
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            System.out.println(number);
+            number= number +1;
+        } */
+
+        for(int number2= 1; number2 <=10; number2++){
+            for (int i = 0; i<number2; i++) {
+            System.out.println(number2) ; }}
+        }
+
+    }
