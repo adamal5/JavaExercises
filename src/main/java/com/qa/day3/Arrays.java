b8321c6637f4d56620129c8f0858a77039ca58d6
Adama <akabba@qa.com>
2020-08-12 14:57:29 +0100

    populated array with values

--- src/main/java/com/qa/day3/Arrays.java
+++ src/main/java/com/qa/day3/Arrays.java
@@ -2,5 +2,20 @@ package com.qa.day3;
 
 public class Arrays {
 
-    public int [] numArray = new int[10];
+    public static int [] numArray = new int[10];
+
+    public static void main(String[] args) {
+        numArray[0]= 1;
+        numArray[1]= 2;
+        numArray[2]= 3;
+        numArray[3]= 4;
+        numArray[4]= 1;
+        numArray[5]= 15;
+        numArray[6]= 1;
+        numArray[7]= 16;
+        numArray[8]= 14;
+        numArray[9]= 12;
+        numArray[10]= 11;
+
+    }
 }
