af4c4cc6874738a8d0b9c33af684a2eee42c0fa0
Adama <akabba@qa.com>
2020-08-12 14:49:46 +0100

    CReated a new  array that holds 10 numbers

---
+++ src/main/java/com/qa/day3/Arrays.java
@@ -0,0 +1,6 @@
+package com.qa.day3;
+
+public class Arrays {
+
+    public int [] numArray = new int[10];
+}
---
+++ src/main/java/com/qa/day3/Coins.java
@@ -0,0 +1,14 @@
+package com.qa.day3;
+
+public class Coins {
+
+    public static void getChange(double cost, double amount){
+        double change = amount - cost;
+        int intPart = (int) change;
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
