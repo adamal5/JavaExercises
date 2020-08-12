69eb80147fbc3a75dfd05bf6612248dbf3bd1b3c
Adama <akabba@qa.com>
2020-08-12 10:18:15 +0100

    added getPercentage AND return value based on pass and fail at 60% overall and individual exam

--- src/main/java/com/qa/day2/Results.java
+++ src/main/java/com/qa/day2/Results.java
@@ -1,16 +1,39 @@
 package com.qa.day2;
 
 public class Results {
-    int physics;
-    int chemistry;
-    int biology;
-    int total;
-    int percentage;
+
+    public static double physics;
+    public static double chemistry;
+    public static double biology;
+    public static double total;
+    public static double percentage;
 
     public static void getMark(){
-        System.out.println("You got " + physics + "in your physics exam.");
-        System.out.println("You got " + chemistry + "in your chemistry exam.");
-        System.out.println("You got " + biology + "in your biology exam.");
-        System.out.println("You got" + total " in total.");
+        System.out.println("You got " + physics + " in your physics exam.");
+        System.out.println("You got " + chemistry + " in your chemistry exam.");
+        System.out.println("You got " + biology + " in your biology exam.");
+        System.out.println("You got " + total + " in total.");
+    }
+
+    public static void getPercentage(){
+        percentage = (total / 450) * 100;
+        double bioPercent = (biology/150) *100;
+        double chemPercent = (chemistry/150) *100;
+        double physPercent = (physics/150) *100;
+        System.out.println("Your overall percentage is " + percentage);
+        if (percentage >= 60 && bioPercent >=60 && chemPercent >=60 && physPercent >=60){
+            System.out.println("You passed!");
+        } else {
+            System.out.println("You've failed, please contact your department");
+        }
+    }
+
+    public static void main(String[] args) {
+        physics = 140;
+        chemistry = 50;
+        biology = 100;
+        total = biology +chemistry + physics;
+        getMark();
+        getPercentage();
     }
 }
