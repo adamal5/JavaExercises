55490f2e8614fdf79282c1e27133343d0ba76e31
Adama <akabba@qa.com>
2020-08-19 16:07:05 +0100

    Populated map string with morse values

---
+++ src/main/java/com/qa/day7/MorseTranslator.java
@@ -0,0 +1,42 @@
+package com.qa.day7;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MorseTranslator {
+    Map<String, String> morseMap;
+
+    public void populateMorseMap() {
+        morseMap = new HashMap<>();
+
+        morseMap.put(".-", "a");
+        morseMap.put("-...", "b");
+        morseMap.put("-.-.", "c");
+        morseMap.put("-..", "d");
+        morseMap.put(".", "e");
+        morseMap.put("..-.", "f");
+        morseMap.put("--.", "g");
+        morseMap.put("....", "h");
+        morseMap.put("..", "i");
+        morseMap.put(".---", "j");
+        morseMap.put("-.-", "k");
+        morseMap.put(".-..", "l");
+        morseMap.put("--", "m");
+        morseMap.put("-.", "n");
+        morseMap.put("---", "o");
+        morseMap.put(".--.", "p");
+        morseMap.put("--.-", "q");
+        morseMap.put(".-.", "r");
+        morseMap.put("...", "s");
+        morseMap.put("-", "t");
+        morseMap.put("..-", "u");
+        morseMap.put("...-", "v");
+        morseMap.put(".--", "w");
+        morseMap.put("-..-", "x");
+        morseMap.put("-.--", "y");
+        morseMap.put("--..", "z");
+        morseMap.put("/", " ");
+    }
+
+}
