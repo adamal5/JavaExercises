c3097621f16aa7dffc85f7691d361071ea8b5133
Adama <akabba@qa.com>
2020-08-24 17:26:32 +0100

    changed or operator to and to fix bug that was return true even if a wrong character was present

--- src/main/java/com/qa/tddPractice/Email.java
+++ src/main/java/com/qa/tddPractice/Email.java
@@ -3,16 +3,16 @@ package com.qa.tddPractice;
 public class Email {
 
     public static boolean isValidEmail(String email) {
-        boolean boolo = false;
+        boolean boolo;
         if (email.endsWith("@qa.com") || email.endsWith("@gmail.com")) {
-            if (!email.startsWith("@") | !email.startsWith("_") | !email.endsWith(")")) {
+            if (!email.startsWith("@") && !email.startsWith("_") && !email.endsWith(")")) {
                 boolo= true;}
             else {boolo=false;}
         }
-        else {boolo= true;}
+        else {boolo= false;}
         return boolo;
     }
         public static void main (String[]args){
-            System.out.println(isValidEmail("_@gmail.com"));
+            System.out.println(isValidEmail("@gmail.com"));
         }
     }
