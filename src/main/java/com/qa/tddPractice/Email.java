8610912b522b3c31b9f039daa47c38a6fa1dff1c
Adama <akabba@qa.com>
2020-08-24 17:27:54 +0100

    Simplified code to remove nested for loop

--- src/main/java/com/qa/tddPractice/Email.java
+++ src/main/java/com/qa/tddPractice/Email.java
@@ -5,14 +5,12 @@ public class Email {
     public static boolean isValidEmail(String email) {
         boolean boolo;
         if (email.endsWith("@qa.com") || email.endsWith("@gmail.com")) {
-            if (!email.startsWith("@") && !email.startsWith("_") && !email.endsWith(")")) {
-                boolo= true;}
-            else {boolo=false;}
+            boolo= !email.startsWith("@") && !email.startsWith("_") && !email.endsWith(")");
         }
         else {boolo= false;}
         return boolo;
     }
         public static void main (String[]args){
-            System.out.println(isValidEmail("@gmail.com"));
+            System.out.println(isValidEmail("adama@gmail.com"));
         }
     }
